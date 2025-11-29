415046a86f8dc41df2c372292e855320a39a4774
HariKrishnan <krish96882@gmail.com>
2025-11-29 14:27:31 +0530

    Add files via upload

--- event.js
+++ event.js
@@ -115,26 +115,5 @@ document.querySelectorAll("[data-animate]").forEach(el => observer.observe(el));
 
 
 
-/* timer function js code */
 
-const countdownDate = new Date().getTime() + 24*60*60*1000; // 24 hours from now
 
-const countdownFunction = setInterval(() => {
-const now = new Date().getTime();
-const distance = countdownDate - now;
-
-const days = Math.floor(distance / (1000*60*60*24));
-const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-const seconds = Math.floor((distance % (1000*60)) / 1000);
-
-document.getElementById("days").innerText = days.toString().padStart(2,'0');
-document.getElementById("hours").innerText = hours.toString().padStart(2,'0');
-document.getElementById("minutes").innerText = minutes.toString().padStart(2,'0');
-document.getElementById("seconds").innerText = seconds.toString().padStart(2,'0');
-
-if (distance < 0) {
-  clearInterval(countdownFunction);
-  document.querySelector(".countdown").innerHTML = "EXPIRED";
-}
-}, 1000);
